4f1bb9560587129be2adf4408403f19c381b0862
mudasir-restoguru <mudasir@resto.guru>
2020-06-29 01:42:37 +0530

    changes 4

--- src/App.js
+++ src/App.js
@@ -35,7 +35,14 @@ const LanguageSwitcher = lazy(() =>
   )
 );
 
-
+const schemaMarkup = {
+  '@context': 'http://schema.org/',
+  '@type': 'NGO',
+  name: 'Coronavirus Outbreak in India: Latest Map and Case Count',
+  alternateName: 'COVID-19 Tracker',
+  url: 'https://www.covid19india.org/',
+  image: 'https://www.covid19india.org/thumbnail.png',
+};
 
 function App() {
   const darkMode = useDarkMode(false);
@@ -46,7 +53,7 @@ function App() {
       pageLink: '/',
       view: State,
       displayName: 'State',
-      showInNavbar: false,
+      showInNavbar: true,
     },
     {
       pageLink: '/demographics',
@@ -82,7 +89,11 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Helmet>
+        <script type="application/ld+json">
+          {JSON.stringify(schemaMarkup)}
+        </script>
+      </Helmet>
 
       <Suspense fallback={<div />}>
         <LanguageSwitcher
@@ -93,7 +104,11 @@ function App() {
       <Suspense fallback={<div />}>
         <Router>
           <ScrollToTop />
-
+          <Navbar
+            pages={pages}
+            {...{darkMode}}
+            {...{showLanguageSwitcher, setShowLanguageSwitcher}}
+          />
           <Route
             render={({location}) => (
               <React.Fragment>
